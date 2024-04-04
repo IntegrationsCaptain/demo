ee6aa31c2746cb3a1f85b8fbd0f9668e295ff5e7
Max Yankov <golergka@gmail.com>
2024-04-03 23:50:11 -0300

    iframe integration

---
+++ app/iframe/page.tsx
@@ -0,0 +1,10 @@
+export default function IFrame() {
+  return (
+    <iframe
+      src="http://demo.localhost:3002"
+      className="w-full h-full border-none"
+      allow="clipboard-write"
+      loading="lazy"
+    />
+  );
+}
