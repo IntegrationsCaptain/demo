34612546034a56433e837e80a8cc54e24028f3db
Max Yankov <golergka@gmail.com>
2024-04-05 22:44:35 -0300

    copy review comments

--- app/iframe/page.tsx
+++ app/iframe/page.tsx
@@ -3,7 +3,7 @@ export default function DemoIFrame() {
   return (
     <div className="h-full w-full flex flex-col bg-gray-200 p-4 shadow-md">
       <div className="mb-2 p-2 bg-gray-300 rounded text-black text-lg font-semibold">
-        iFrame integration Demo
+        Embedding with an iFrame
       </div>
       <div className="flex-grow bg-white p-2 rounded">
         <iframe
--- app/react/page.tsx
+++ app/react/page.tsx
@@ -5,7 +5,7 @@ export default function DemoReact() {
   return (
     <div className="h-full w-full flex flex-col bg-gray-200 p-4 shadow-md">
       <div className="mb-2 p-2 bg-gray-300 rounded text-black text-lg font-semibold">
-        React integration Demo
+        Embedding with a React component
       </div>
       <div className="flex-grow bg-white p-2 rounded">
         <Store url={STORE_URL} />
