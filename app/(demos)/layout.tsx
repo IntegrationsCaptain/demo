02b25fdae0fdb690c8eb1f77a823f28a79603e96
Max Yankov <golergka@gmail.com>
2024-04-09 21:57:32 -0300

    NavMenu on top

--- app/(demos)/layout.tsx
+++ app/(demos)/layout.tsx
@@ -1,14 +1,14 @@
+import NavMenu from "@/components/NavMenu";
+
 export default function DemoLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="h-full w-full flex flex-col bg-gray-200 p-4 shadow-md">
-      <div className="mb-2 p-2 bg-gray-300 rounded text-black text-lg font-semibold">
-        Embedding demo
-      </div>
-      <div className="flex-grow bg-white p-2 rounded">{children}</div>
+    <div className="h-full w-full flex flex-col">
+      <NavMenu />
+      <div className="flex-grow">{children}</div>
     </div>
   );
 }
