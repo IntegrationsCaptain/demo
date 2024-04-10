9a0b8979043e49b4252b2f60a7e766b3efcaa021
Max Yankov <golergka@gmail.com>
2024-04-09 22:02:31 -0300

    updated main page design

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,28 @@
+import { paths } from "@/lib/paths";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col p-24">
-      <h1 className="text-2xl font-bold mb-4">Integrations Captain Demo App</h1>
-      <p className="mb-4">
-        This app showcases different ways to embed IntegrationsCaptain store.
+      <h1 className="text-3xl font-semibold mb-6">
+        Integrations Captain Demo App
+      </h1>
+      <p className="mb-8">
+        Explore the various ways to integrate the IntegrationsCaptain store into
+        your application.
       </p>
-      <ul className="list-disc list-inside shadow-md rounded-lg p-4">
-        <li className="mb-2">
-          <Link
-            href="/iframe"
-            className="text-blue-600 hover:text-blue-800 visited:text-purple-600"
-          >
-            iFrame integration
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/react"
-            className="text-blue-600 hover:text-blue-800 visited:text-purple-600"
-          >
-            React: basic integration
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/react_path_change"
-            className="text-blue-600 hover:text-blue-800 visited:text-purple-600"
-          >
-            React: react to path changes
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/react_install_status"
-            className="text-blue-600 hover:text-blue-800 visited:text-purple-600"
-          >
-            React: pass install status
-          </Link>
-        </li>
-      </ul>
+      <div>
+        {paths.map((link) => (
+          <div key={link.href} className="mb-2">
+            <Link
+              href={link.href}
+              className="text-sm text-gray-800 hover:text-gray-500 transition-colors"
+            >
+              {link.name}
+            </Link>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
