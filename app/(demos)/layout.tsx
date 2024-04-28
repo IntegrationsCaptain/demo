f848b7eebeb58c0b2d0b7370d88f8705323be663
Koby Soto <koby@integrationscaptain.com>
2024-04-28 09:59:12 -0700

    layout simplification

--- app/(demos)/layout.tsx
+++ app/(demos)/layout.tsx
@@ -1,9 +1,10 @@
+import Header from '@/components/Header';
 import NavMenu from '@/components/NavMenu';
 
 export default function DemoLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<div className="flex h-full w-full flex-col">
-			<NavMenu />
+			<Header />
 			<div className="flex-grow">{children}</div>
 		</div>
 	);
--- lib/paths.ts
+++
@@ -1,6 +0,0 @@
-export const paths: { href: string; name: string }[] = [
-	{ href: '/iframe', name: 'iFrame' },
-	{ href: '/react', name: 'Basic React' },
-	{ href: '/react_install_status', name: 'Manage install status' },
-	{ href: '/react_path_change', name: 'React to path changes' },
-];
