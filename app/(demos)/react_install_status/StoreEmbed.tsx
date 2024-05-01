769e6417274d7d264e6819dca12794237621f060
Koby Soto <koby@integrationscaptain.com>
2024-05-01 15:30:47 -0700

    unmanaged purchases page

--- app/(demos)/react_install_status/StoreEmbed.tsx
+++ app/(demos)/react_install_status/StoreEmbed.tsx
@@ -1,29 +1,39 @@
 'use client';
 import { z } from 'zod';
 import { CallbackEvent, Store } from '@sidedish/react';
+import { toast } from 'sonner';
+import { useState } from 'react';
 
 const callbackEventSchema = z.object({
 	listingId: z.string(),
 });
 
-export const StoreEmbed = ({ installed }: { installed: string[] }) => {
+export const StoreEmbed = ({ installedListings }: { installedListings: string[] }) => {
+	const [installed, setInstalled] = useState(installedListings);
 	const onCallback = (e: CallbackEvent) => {
-		if (e.actionIdentifier === 'INSTALL_LISTING') {
+		if (e.actionIdentifier === 'INSTALL') {
 			// Do whatever you want.
 			const payload = callbackEventSchema.parse(e.payload);
-			alert('installed listing with id: ' + payload.listingId);
+			toast.success('asked to install listing with id: ' + payload.listingId);
 
-			// 1) If the callback has caused a change in the unsafe params, you can just update the passed `unsafeParams` and the component
+			// HERE you would do your own logic to install the app. For example, you could call an internal API to install the app.
+			// fetch('/your-api/install', {listingId: payload.listingId}) //this is just an example.
+
+			// The callback has caused a change in the unsafe params, we can just update the passed `unsafeParams` and the component
 			// will take care of posting an update event for you. This is the same as doing:
 			// storeRef.current.updateUnsafeParams({}) but easier to just update `unsafeParams`
+			setInstalled([...installed, payload.listingId]);
+		} else if (e.actionIdentifier === 'UNINSTALL') {
+			// Do whatever you want.
+			const payload = callbackEventSchema.parse(e.payload);
+			toast.success('asked to uninstall listing with id: ' + payload.listingId);
 
-			// 2) If there was a change in data that is included in the safe session, you can ask the store to reload the session:
-			// if (storeRef.current){
-			// 	storeRef.current.reloadSession();
-			// }
+			// HERE you would do your own logic to uninstall the app. For example, you could call an internal API to uninstall the app.
+			// fetch('/your-api/uninstall', {listingId: payload.listingId}) //this is just an example.
+			setInstalled(installed.filter((id) => id !== payload.listingId));
 		}
 	};
 
 	const STORE_URL = process.env.NEXT_PUBLIC_STORE_URL || 'https://demo.integrations.store';
-	return <Store url={STORE_URL} unsafeParams={{ installed }} onCallback={onCallback} />;
+	return <Store url={STORE_URL} unsafeParams={{ purchases: installed }} onCallback={onCallback} />;
 };
